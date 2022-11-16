63ec4215e908d3243d69499eb7d7a74bb18fe1ba
Aurimas111 <aurimas.civ@gmail.com>
2022-11-16 16:32:01 +0200

    slider

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
 import React, { useEffect } from 'react'
+import SliderWindow from './Components/SliderWindow';
 
 
 function App() {
   const [userStakeKey, setStakeKey] = React.useState()
 
+
+
   return (
     <div className="App">
       <Header setStakeKey={setStakeKey}></Header>
+      <SliderWindow></SliderWindow>
       {userStakeKey != null ? <h1>{userStakeKey}</h1> : <h1>connect your wallet</h1>}
+
       <Footer></Footer>
     </div>
   );
---
+++ src/Components/SliderWindow.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+
+export default function SliderWindow() {
+    const [sliderValue, setSliderValue] = React.useState(1);
+
+
+     const changeValue = (event, value) => {
+        setSliderValue(parseInt(event.target.value));
+      };
+
+    return (
+
+        <div className="slider-container">
+          <div className='slideris' style={{ marginTop: "250px", marginBottom: "130px" }}>
+              <div className='frame-1'>
+                <div className='rectangle-2'>
+                  <div className='supported-wallets'>
+                    <span className='inter-bold-white-20px'>Currently only Nami and Eternl wallets are supported!</span>
+                  </div>
+                  <div className='information-1'>
+                    <span>Cost is 80 ADA per NFT.</span>
+                  </div>
+                  <div className='information-2'>
+                    <span>Please select the amount of NFTs you would like to mint using the slider below.</span>
+                  </div>
+                </div>
+                <Box sx={{ width: 500 }} className="slider-itself" style={{ marginTop: "300px" }}>
+                  <Slider
+                    aria-label="Always visible"
+                    defaultValue={1}
+                    //getAriaValueText={getText}
+                    step={1}
+                    //marks={marks}
+                    min={1}
+                    max={5}
+                    valueLabelDisplay="on"
+                    color="primary"
+                    value={sliderValue}
+                    onChange={changeValue}
+                    />
+
+                </Box>
+                <button className="btn btn-success" onClick={() => this.handleNextButton(this.state.value)}>Next</button>
+                {sliderValue}
+
+              </div>
+            </div>
+            </div>
+            
+
+    )
+}
